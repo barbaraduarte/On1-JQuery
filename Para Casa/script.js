8dc7cdfb42c95675d0acd33197abdd4d05ad6b65
barbaraduarte <barbara.silveira@live.com>
2019-04-18 22:52:17 -0300

    exercicio on1 jquery

---
+++ Para Casa/script.js
@@ -0,0 +1,50 @@
+$(function(){
+
+    $('#carousel img:eq(0)').addClass('ativo').show();
+    let legenda = $('.ativo').attr('alt');
+    $('#carousel').prepend("<p>"+legenda+"</p>");
+    setInterval(slider,2500);
+
+    function slider(){
+
+        if($('.ativo').next().length){
+
+            $('.ativo').fadeOut().removeClass('ativo').next().fadeIn().addClass('ativo');
+
+        }else{
+            $('.ativo').fadeOut().removeClass('ativo');
+            $('#carousel img:eq(0)').fadeIn().addClass('ativo');
+
+        }
+
+        let legenda = $('.ativo').attr('alt');
+        $('#carousel p').hide().html(legenda).delay(500).fadeIn();
+
+    }
+});
+
+
+// //Click
+// $(function(){
+
+//     $('#carousel img:eq(0)').addClass('ativo').show();
+//     let legenda = $('.ativo').attr('alt');
+//     $('#carousel').prepend("<p>"+legenda+"</p>");
+
+//     $('#carousel').click(function(){
+
+//         if($('.ativo').next().length){
+
+//             $('.ativo').fadeOut().removeClass('ativo').next().fadeIn().addClass('ativo');
+
+//         }else{
+//             $('.ativo').fadeOut().removeClass('ativo');
+//             $('#carousel img:eq(0)').fadeIn().addClass('ativo');
+
+//         }
+
+//         let legenda = $('.ativo').attr('alt');
+//         $('#carousel p').hide().html(legenda).delay(500).fadeIn();
+
+//     })
+// });
